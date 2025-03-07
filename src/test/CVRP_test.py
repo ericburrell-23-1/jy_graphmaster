de4fd1fa1e1f95458f6a199edb76dbdbb0b5edae
julian.yarkony <julian@DLT210020.lan>
2025-03-06 17:13:09 -0800

    generate_error_fix

--- src/test/CVRP_test.py
+++ src/test/CVRP_test.py
@@ -15,6 +15,7 @@ class CVRPTest(TestCase):
     def test_thirty_customers(self):
         try:
             instance = "NYC3"
+            instance = "10_customers"
             #instance = "prob_inst_33"
             problem = CVRP(
                 os.path.join(os.path.dirname(__file__),
