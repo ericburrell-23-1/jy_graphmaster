70e081ab811c75ba14d157e9fe66109a20689836
julian.yarkony <julian@DLT210020.lan>
2025-03-21 14:49:43 -0700

    hi

--- src/common/jy_fast_pricing.py
+++ src/common/jy_fast_pricing.py
@@ -13,3 +13,17 @@ from src.algorithm.jy_slow_pricing import SortedObjectList
 from jy_eff_fronteir import jy_efficient_frontier
 from jy_label import jy_label
 from jy_sortedObject_list import jy_sortedObject_list
+
+class jy_fast_pricing():
+
+
+    def __init__(self,all_actions,dual_vec,init_res_state,max_actions_in_route,actions_of_node,all_nodes,jy_opt):
+
+        self.all_actions=all_actions
+        self.dual_vec_init=dual_vec.copy()
+        self.dual_vec_init=dual_vec.copy()
+        self.init_res_state=init_res_state
+        self.max_actions_in_route=max_actions_in_route
+        self.actions_of_node=actions_of_node
+        self.all_nodes=all_nodes
+        self.jy_opt=jy_opt
