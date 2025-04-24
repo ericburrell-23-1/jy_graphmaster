75e2f8669502074dc60fe51844355ac3f9cc240c
xiaohang.zhu <xiaohang.zhu@optym.com>
2025-04-24 18:40:30 -0400

    another push

--- src/test/LoadAI_cg_test.py
+++ src/test/LoadAI_cg_test.py
@@ -14,7 +14,7 @@ class CVRPTest(TestCase):
     def test_thirty_customers(self):
         run_model=True
         
-        instance_name = "uhual_3178"
+        instance_name = "uhual_200"
 
         path = os.path.join(os.path.dirname(__file__),
                         "assets", "loadai_instances", f"{instance_name}.csv")
